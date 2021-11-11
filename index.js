de90640733254d3084e61b01c8dfeee6f15291f5
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-11-11 20:32:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 let fs = require('fs')
-let mainF = require('./main')
-//let CFonts = require('cfonts')
+let msgMain = require('./msg/message')
+let CFonts = require('cfonts')
 let figlet = require('figlet')
+let { color } = require('./lib/color')
+let package = JSON.parse(fs.readFileSync('./package.json'))
+
+function start() {
+  console.log(color(figlet.textSync(`Cxd9Bot`, 'Larry 3D'), 'cyan'))
+  CFonts.say(`Created By : ${package.author} Team!`, {
+    font: 'console',
+    align: "center",
+    gradient: ['yellow', 'yellow']
+  })
+  msgMain()
+}
+
+start()
