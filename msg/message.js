ae420fe71d0cacd6ef6c9f0c64050a786d947e10
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-11-14 23:13:18 +0700

    Update message.js

--- msg/message.js
+++ msg/message.js
@@ -1,8 +1,83 @@
 let Baileys = require('@adiwajshing/baileys')
 let { color } = require('./../lib/color')
 let fs = require('fs')
+let _scommand = JSON.parse(fs.readFileSync("./database/scommand.json"))
+
 require('./../setting')
 
-module.exports = msgMain = (CXD, message) => {
-  console.log('test')
+module.exports = msgMain = (CXD = new conn, msg) => {
+  try {
+    if (!msg.hasNewMessage) return
+    msg = msg.messages.all()[0]
+    if (!msg.message) return
+    if (msg.key && msg.key.remoteJid == 'status@broadcast') return
+    msg.message = (Object.keys(msg.message)[0] === 'ephemeralMessage') ? msg.message.ephemeralMessage.message: msg.message
+    let content = JSON.stringify(msg.message)
+    let from = msg.key.remoteJid
+    let {
+      text,
+      extendedText,
+      contact,
+      location,
+      liveLocation,
+      image,
+      video,
+      sticker,
+      document,
+      audio,
+      product,
+      buttonsMessage
+    } = Baileys.MessageType
+    type = Object.keys(msg.message)[0]
+    cmd = type === "conversation" && mek.message.conversation ? mek.message.conversation: type == "imageMessage" && mek.message.imageMessage.caption ? mek.message.imageMessage.caption: type == "videoMessage" && mek.message.videoMessage.caption ? mek.message.videoMessage.caption: type == "extendedTextMessage" && mek.message.extendedTextMessage.text ? mek.message.extendedTextMessage.text: type == "buttonsResponseMessage" && mek.message[type].selectedButtonId ? mek.message[type].selectedButtonId: ""
+    let getCmd = (id) => { let position = null
+      Object.keys(_scommand).forEach((i) => {
+        if (_scommand[i].id === id) {
+          position = i
+        }
+      })
+      if (position !== null) {
+        return _scommand[position].chats
+      }
+    }
+    let prefix = global.botConf.prefix[0]
+    body = type === 'listResponseMessage' && msg.message.listResponseMessage.title ? msg.message.listResponseMessage.title: type == 'buttonsResponseMessage' && msg.message.buttonsResponseMessage.selectedButtonId ? mek.message.buttonsResponseMessage.selectedButtonId: type == "conversation" && mek.message.conversation.startsWith(prefix) ? mek.message.conversation: type == "imageMessage" && mek.message.imageMessage.caption.startsWith(prefix) ? mek.message.imageMessage.caption: type == "videoMessage" && mek.message.videoMessage.caption.startsWith(prefix) ? mek.message.videoMessage.caption: type == "extendedTextMessage" && mek.message.extendedTextMessage.text.startsWith(prefix) ? mek.message.extendedTextMessage.text: ""
+    let chats = (type === 'conversation') ? mek.message.conversation: (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text: ''
+    listbut = (type == 'listResponseMessage') ? msg.message.listResponseMessage.title: ''
+    let args = body.trim().split(/ +/).slice(1)
+    let isCmd = body.startsWith(prefix)
+    let q = args.join(' ')
+    let botNumber = CXD.user.jid
+    let isGroupMsg = from.endsWith("@g.us")
+    let sender = isGroupMsg ? msg.participant : msg.key.remoteJid
+    let groupMetadata = isGroupMsg ? CXD.groupMetadata(from) : ''
+    let groupName = isGroupMsg ? groupMetadata.subject : ''
+    let groupId = isGroupMsg ? groupMetadata.jid : ''
+    let groupMembers = isGroupMsg ? groupMetadata.participants : ''
+
+    CXD.reply = (txt) => {
+      CXD.sendMessage(
+        from, txt, text, {
+          quoted: msg
+        }
+      )
+    }
+
+    global.pushname = MUT.contacts[sender] != undefined ? MUT.contacts[sender].vname || MUT.contacts[sender].notify : undefined
+
+    let isMedia = type === "imageMessage" || type === "videoMessage"
+    let isQuotedImage = type === "extendedTextMessage" && content.includes("imageMessage")
+    let isQuotedVideo = type === "extendedTextMessage" && content.includes("videoMessage")
+    let isQuotedAudio = type === "extendedTextMessage" && content.includes("audioMessage")
+    let isQuotedSticker = type === "extendedTextMessage" && content.includes("stickerMessage")
+    if (isCmd && isGroupMsg) console.log('[CXD]', command, 'from', sender.split('@')[0], 'args :', args.length)
+    if (!isGroupMsg && isCmd) console.log('[CXD]', command, 'from', sender.split('@')[0], 'args :', args.length)
+    switch(body) {
+      case prefix + 'p':
+        CXD.reply('Test')
+      break
+    }
+  } catch(err) {
+    console.log(color("Error:", "red"), err)
+  }
 }
