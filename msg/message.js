8b93f16a6bd656c12831e765bb489973f0be8227
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-11-14 22:50:46 +0700

    Update message.js

--- msg/message.js
+++ msg/message.js
@@ -1,5 +1,7 @@
-require('./../setting')
 let Baileys = require('@adiwajshing/baileys')
+let { color } = require('./../lib/color')
+let fs = require('fs')
+require('./../setting')
 
 module.exports = msgMain = (CXD, message) => {
   console.log('test')
