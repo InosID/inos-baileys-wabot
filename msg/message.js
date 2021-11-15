3d2e607cf5861473441e67609ae704f8c8676f94
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-11-15 10:43:25 +0700

    Update message.js

--- msg/message.js
+++ msg/message.js
@@ -1,12 +1,13 @@
 let Baileys = require('@adiwajshing/baileys')
 let fs = require('fs')
 let axios = require('axios')
+let get = require('got')
 
+let fetcher = require('./../lib/fetcher')
 let { color } = require('./../lib/color')
 let _scommand = JSON.parse(fs.readFileSync("./database/scommand.json"))
 
 require('./../config')
-require('./sendMsg')
 
 module.exports = msgMain = (CXD = new conn, msg) => {
   try {
@@ -59,6 +60,182 @@ module.exports = msgMain = (CXD = new conn, msg) => {
     let groupId = isGroupMsg ? groupMetadata.jid : ''
     let groupMembers = isGroupMsg ? groupMetadata.participants : ''
 
+    /**
+     * Reply message
+     * @param {string} txt
+     */
+    global.reply = (from, txt) => {
+      CXD.sendMessage(
+        from,
+        txt,
+        text, {
+          quoted: msg
+        }
+      )
+    }
+
+    /**
+     * Send text
+     * @param {string} txt
+     */
+    global.sendText = (from, txt) => {
+      CXD.sendMessage(
+        from,
+        txt,
+        text
+      )
+    }
+
+    /**
+     * Send Image File
+     * @param {object} img
+     * @param {string} caption
+     * @param {boolean} replying
+     */
+    global.sendImage = (from, img, caption, replying) => {
+      if (replying = true) {
+        CXD.sendMessage(
+          from,
+          img,
+          image, {
+            caption: caption,
+            quoted: msg
+          }
+        )
+      } else {
+        CXD.sendMessage(
+          from,
+          img,
+          image, {
+            caption: caption
+          }
+        )
+      }
+    }
+
+    /**
+     * Send file
+     * @param {object} file
+     * @param {string} type
+     * @param {string} captions
+     * @param {boolean} replying
+     */
+    global.sendFile = (from, file, type, captions, replying) => {
+      if (type == 'document') {
+        if (replying == true) {
+          CXD.sendMessage(
+            from,
+            file,
+            type, {
+              quoted: msg
+            }
+          )
+        } else {
+          CXD.sendMessage(
+            from,
+            file,
+            type
+          )
+        }
+      } else if (type == 'image') {
+        if (replying == true) {
+          CXD.sendMessage(
+            from,
+            file,
+            type, {
+              caption: captions,
+              quoted: msg
+            }
+          )
+        } else {
+          CXD.sendMessage(
+            from,
+            file,
+            type, {
+              caption: captions
+            }
+          )
+        }
+      } else if (type === 'video') {
+        if (replying == true) {
+          CXD.sendMessage(
+            from,
+            file,
+            type, {
+              caption: captions,
+              quoted: msg
+            }
+          )
+        } else {
+          CXD.sendMessage(
+            from,
+            file,
+            type, {
+              caption: captions
+            }
+          )
+        }
+      } else if (type == 'audio') {
+        if (replying == true) {
+          CXD.sendMessage(
+            from,
+            file,
+            type, {
+              mimetype: 'audio/mp4',
+              quoted: msg
+            }
+          )
+        } else {
+          CXD.sendMessage(
+            from,
+            file,
+            type, {
+              mimetype: 'audio/mp4',
+            }
+          )
+        }
+      } else if (type == 'gif') {
+        if (replying == true) {
+          CXD.sendMessage(
+            from,
+            file,
+            video, {
+              mimetype: Baileys.Mimetype.gif,
+              quoted: msg,
+              caption: captions
+            }
+          )
+        }
+      }
+    }
+
+    /**
+     * Send file from url
+     * @param {string} url
+     * @param {string} type
+     * @param {string} captions
+     * @param {boolean} replying
+     */
+    global.sendFileFromUrl = (from, url, type, captions, replying) => {
+      if (type == 'document') {
+        var fetch = global.buffer(url)
+        if (replying == true) {
+          CXD.sendMessage(
+            from,
+            url,
+            type, {
+              quoted: msg
+            }
+          )
+        } else {
+          CXD.sendMessage(
+            from,
+            url,
+            type
+          )
+        }
+      }
+    }
     CXD.reply = global.reply
     CXD.sendImage = global.sendImage
     CXD.sendFile = global.sendFile
