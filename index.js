fb2aa4dc0289cc6b8a5940b6ecf3e142f94ca51b
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-11-18 21:51:18 +0700

    Haa
    Hm

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ let package = JSON.parse(fs.readFileSync('./package.json'))
 let express = require('express')
 let app = new express()
 let request = require('request')
-let qrcode = require('qrcode')
 
 app.get('/', (req, res) => res.status(200).send('CXD Client'))
 let PORT = process.env.PORT || 8080 || 5000 || 3000
