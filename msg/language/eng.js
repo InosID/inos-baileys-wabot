fcd3c3a14cd3ee9ba55a1300858fd4559d20dcc1
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-11-22 17:57:32 +0700

    Update eng.js

--- msg/language/eng.js
+++ msg/language/eng.js
@@ -1,4 +1,11 @@
+// Actions
 exports.wait = () => {
   return
     `[⏳] Wait a moment...`
+}
+
+// Permission
+exports.notAllowed = () => {
+  return
+    `[❕] This command is not permitted by the owner.`
 }
