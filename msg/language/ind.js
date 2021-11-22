c0be40d517c023829c44afc7dad30b59ab886a7b
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-11-22 17:55:18 +0700

    Update ind.js

--- msg/language/ind.js
+++ msg/language/ind.js
@@ -1,4 +1,11 @@
+// Actions
 exports.wait = () => {
   return
     `[⏳] Tunggu sebentar...`
+}
+
+// Permission
+exports.notAllowed = () => {
+  return
+    `[❕] Perintah ini tidak diizinkan oleh owner.`
 }
