918bc879dd0366316fe24cf2080e458708ec8617
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-11-23 23:13:44 +0700

    Salah

--- msg/message.js
+++ msg/message.js
@@ -244,7 +244,7 @@ module.exports = msgMain = async(CXD = new conn, msg) => {
       break
       case 'nsfwanime':
         if (allow.nsfw) return CXD.reply(mess.notAllowed())
-        if (!isNsfw) return CXD.reply(mess.nsfwOff())
+        if (isNsfw) return CXD.reply(mess.nsfwOff())
         CXD.reply(mess.wait())
         nsfwanime.result()
           .then(async (res) => {
