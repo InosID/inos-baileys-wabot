196b6efaf2272e4b7267ba4bab5a31d3ccd99b21
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-11-23 15:13:15 +0700

    Haah?
    #misahin

---
+++ msg/command/primbon/artinama.js
@@ -0,0 +1,16 @@
+let axios = require("axios")
+let cheerio = require("cheerio")
+
+async function artiNama(nama) {
+  return new Promise(async (resolve, reject) => {
+    await axios.get(`https://www.primbon.com/arti_nama.php?nama1=${nama}&proses=+Submit%21+`)
+      .then(({ data }) => {
+        const $ = cheerio.load(data)
+        const isi = $("#body").text().split("Nama:")[0]
+        const result = isi.replace(/\n/gi, "").replace("ARTI NAMA", "").replace("                                ", "");
+        resolve(result)
+      }).catch(reject)
+  });
+};
+
+module.exports = { artiNama }
