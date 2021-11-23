d796d0ba560989a76ca9f1e00e4fad345d6ba1bf
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-11-23 16:01:33 +0700

    Create kecocokanNama.js

---
+++ msg/command/primbon/kecocokanNama.js
@@ -0,0 +1,31 @@
+/**
+ * Kecocokan nama scrapper by @Fxc7
+ */
+async function result(nama, tanggal) {
+  return new Promise(async (resolve, reject) => {
+    const tgl = Tanggal(tanggal).tanggal;
+    const bln = Tanggal(tanggal).bulan;
+    const thn = Tanggal(tanggal).tahun;
+    const options = {
+      method: 'POST',
+      url: 'https://primbon.com/kecocokan_nama.php',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      data: new URLSearchParams(Object.entries({
+        nama,
+        tgl,
+        bln,
+        thn,
+        kirim: " Submit! "
+      })),
+    };
+    await axios(options).then(({ data }) => {
+      const $ = cheerio.load(data);
+      const result = $('#body').text().replace(/^\s*\n/gm, "").replace("Tgl. Lahir:", "\nTanggal Lahir:").replace(/< Hitung Kembali.*$/s, "")
+      resolve(result)
+    }).catch(reject);
+  })
+}
+
+module.exports = { result }
