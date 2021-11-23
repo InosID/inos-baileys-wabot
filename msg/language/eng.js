2aa2c09a14a01e08014002f65fb36a5c87d27972
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-11-23 10:56:38 +0700

    Update eng.js

--- msg/language/eng.js
+++ msg/language/eng.js
@@ -35,3 +35,6 @@ exports.yt3res = (title, quality, type, size) => {
 exports.nsfwHasOn = () => {
   return `[❕] Nsfw has been active before.`
 }
+exports.nsfwOff = () => {
+  return `[❕] Nsfw is not enabled.`
+}
