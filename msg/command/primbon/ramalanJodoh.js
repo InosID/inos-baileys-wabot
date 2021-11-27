0ee8fcdd4424b6bc930ff926629e4ea207409eb8
Farhannnnn <74123134+Fxc7@users.noreply.github.com>
2021-11-28 00:48:46 +0700

    Update ramalanJodoh.js

--- msg/command/primbon/ramalanJodoh.js
+++ msg/command/primbon/ramalanJodoh.js
@@ -1,20 +1,10 @@
 /**
  * Ramalan jodoh scrapper by @Fxc7
+ * #Jangan Lupa Titik Koma;
  */
-let axios = require('axios')
-let cheerio = require('cheerio')
-
-function Tanggal(tanggal) {
-  const tgl = tanggal.replace(/-.*/, "");
-  const bln = tanggal.replace(/-([^-?]+)(?=(?:$|\?))/, "").replace(/.*?-/, "");
-  const thn = tanggal.replace(/.*\-/, "");
-  const result = {
-    tanggal: tgl,
-    bulan: bln,
-    tahun: thn
-  };
-  return result;
-}
+let axios = require('axios');
+let cheerio = require('cheerio');
+let { Tanggal } = require('./functions');
 
 async function result(nama1, tanggal1, nama2, tanggal2) {
   return new Promise(async (resolve, reject) => {
@@ -52,4 +42,4 @@ async function result(nama1, tanggal1, nama2, tanggal2) {
   });
 }
 
-module.exports = { result }
+module.exports = { result };
