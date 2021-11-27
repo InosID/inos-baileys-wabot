ecd718e98c5203eb86b1bc578391c1f242421d93
Farhannnnn <74123134+Fxc7@users.noreply.github.com>
2021-11-28 00:55:39 +0700

    Update zodiakHar.js

--- msg/command/primbon/zodiakHar.js
+++ msg/command/primbon/zodiakHar.js
@@ -1,12 +1,12 @@
-/**
+/*
  * Zodiak hari ini (keknya) by @Fxc7
  */
-let axios = require('axios')
-let cheerio = require('cheerio')
+let axios = require('axios');
+let cheerio = require('cheerio');
 
-async function result(querry) {
+async function result(query) {
   let Hasil = []
-  await axios(`https://www.fimela.com/zodiak/${querry}`, {
+  await axios(`https://www.fimela.com/zodiak/${query}`, {
     method: "GET",
     headers: {
       "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
@@ -20,7 +20,7 @@ async function result(querry) {
     let thumb = $('body > div > div > div').find('div > div > a > img').attr('src')
     let judul = $('body > div > div.container-main > div.container-article > div').find('div.zodiak--content-header__right > div.zodiak--content-header__text > h5').text().trim()
     let tanggal = $('body > div > div > div > div > div > div > span').text().trim()
-    let nomer_ = $('body > div > div > div > div > div > div').find('div:nth-child(1) > div.zodiak--content__content > span').text().trim()
+    let nomer = $('body > div > div > div > div > div > div').find('div:nth-child(1) > div.zodiak--content__content > span').text().trim()
     let isi = []
     $('body > div > div > div > div > div > div').each(function(a, b) {
       let umum = $(this).find('div:nth-child(1) > div.zodiak--content__content > p').text().trim() || undefined
@@ -33,23 +33,23 @@ async function result(querry) {
       }
       isi.push(Data)
     })
-    let ramal = []
-    isi.map(ryuzin => {
-      if (ryuzin.umum === undefined) return
-      if (ryuzin.love === undefined) return
-      if (ryuzin.keuangan === undefined) return
-      ramal.push(ryuzin)
+    let ramalan = []
+    isi.map(a => {
+      if (a.umum === undefined) return
+      if (a.love === undefined) return
+      if (a.keuangan === undefined) return
+      ramalan.push(ryuzin)
     })
     const result = {
       judul: judul,
       thumb: thumb,
       date: tanggal,
-      no_hoki: nomer_,
-      ramalan: ramal[0]
-    }
-    Hasil.push(result)
-  })
-  return Hasil[0]
+      no_hoki: nomer,
+      ramalan: ramalan[0]
+    };
+    Hasil.push(result);
+  });
+  return Hasil[0];
 }
 
-module.exports = { result }
+module.exports = { result };
