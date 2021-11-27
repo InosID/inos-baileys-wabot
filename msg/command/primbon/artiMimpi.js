9d21c4f2b6e445a113bb77fdc9b9c982a571a727
Farhannnnn <74123134+Fxc7@users.noreply.github.com>
2021-11-28 00:43:45 +0700

    Update artiMimpi.js

--- msg/command/primbon/artiMimpi.js
+++ msg/command/primbon/artiMimpi.js
@@ -10,15 +10,15 @@ async function result(mimpi) {
   return new Promise(async (resolve, reject) => {
     await axios.get(`https://www.primbon.com/tafsir_mimpi.php?mimpi=${mimpi}&submit=+Submit+`)
       .then(({ data }) => {
-        const $ = cheerio.load(data)
-        const cek = $("#body > font > i").text()
-        const adaga = /Tidak ditemukan/g.test(cek) ? false : true
+        const $ = cheerio.load(data);
+        const cek = $("#body > font > i").text();
+        const adaga = /Tidak ditemukan/g.test(cek) ? false : true;
         if (adaga) {
           const isi = $("#body").text().split(`Hasil pencarian untuk kata kunci: ${mimpi}`)[1].replace(/\n\n\n\n\n\n\n\n\n/gi, "\n");
           const result = isi.replace(/\n/gi, "").replace("       ", "").replace("\"        ", "").replace(/Solusi.*$/, "");
           const hasil = {
             result: replaceAll(`${result}`, ".Mimpi", ".\nMimpi")
-          }
+          };
           resolve(hasil);
         } else {
           resolve(`Tidak ditemukan tafsir mimpi ${mimpi}. Cari dengan kata kunci yang lain..`);
