c4faf2315def2bf5bc0942cee0869f6fd8a3f477
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-12-18 15:07:21 +0700

    *merapihkan

--- msg/command/anime/nekonime.js
+++ msg/command/anime/nekonime.js
@@ -5,15 +5,15 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 async function result() {
-return new Promise(async(resolve, reject) => {
-  await axios.get("https://nekos.life")
-  .then(({ data }) => {
-    $ = cheerio.load(data)
-      const result = {
-        image: $("div.w3-modal > div > img").attr("src")
-      }
-      resolve(result)
-    }).catch(reject)
+  return new Promise(async(resolve, reject) => {
+    await axios.get("https://nekos.life")
+      .then(({ data }) => {
+        $ = cheerio.load(data)
+        const result = {
+          image: $("div.w3-modal > div > img").attr("src")
+        }
+        resolve(result)
+      }).catch(reject)
   })
 }
 
