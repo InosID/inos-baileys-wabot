0cc896652631b0cf3886c7b2acbae4cce429f4d1
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-12-29 22:37:05 +0700

    Update testf.js

--- test/testf.js
+++ test/testf.js
@@ -1,45 +1,49 @@
-const resultTst = async (yutub) => {
-function post(url, formdata) {
-    return fetch(url, {
-        method: 'POST',
-        headers: {
-            accept: "*/*",
-            'accept-language': "en-US,en;q=0.9",
-            'content-type': "application/x-www-form-urlencoded; charset=UTF-8"
-        },
-        body: new URLSearchParams(Object.entries(formdata))
+async function resultpl(query) {
+    return new Promise((resolve, reject) => {
+        try {
+            const search = yts(query)
+            .then((data) => {
+                const url = []
+                const pormat = data.all
+                for (let i = 0; i < pormat.length; i++) {
+                    if (pormat[i].type == 'video') {
+                        let dapet = pormat[i]
+                        url.push(dapet.url)
+                    }
+                }
+                const id = yt.getVideoID(url[0])
+                const yutub = yt.getInfo(`https://www.youtube.com/watch?v=${id}`)
+                .then((data) => {
+                    let pormat = data.formats
+                    let audio = []
+                    let video = []
+                    for (let i = 0; i < pormat.length; i++) {
+                    if (pormat[i].mimeType == 'audio/webm; codecs=\"opus\"') {
+                        let aud = pormat[i]
+                        audio.push(aud.url)
+                    }
+                    }
+                    const title = data.player_response.microformat.playerMicroformatRenderer.title.simpleText
+                    const thumb = data.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url
+                    const channel = data.player_response.microformat.playerMicroformatRenderer.ownerChannelName
+                    const views = data.player_response.microformat.playerMicroformatRenderer.viewCount
+                    const published = data.player_response.microformat.playerMicroformatRenderer.publishDate
+                    const result = {
+                    title: title,
+                    thumb: thumb,
+                    channel: channel,
+                    published: published,
+                    views: views,
+                    url: audio[0]
+                    }
+                    return(result)
+                })
+                return(yutub)
+            })
+            resolve(search)
+        } catch (error) {
+            reject(error)
+        }
+        console.log(error)
     })
-}
-
-let ytId = ytIdRegex.exec(yutub)
-url = 'https://youtu.be/' + ytId[1]
-let res = await post(`https://www.y2mate.com/mates/en68/analyze/ajax`, {
-    url,
-    q_auto: 0,
-    ajax: 1
-  })
-const mela = await res.json()
-const $ = cheerio.load(mela.result)
-const hasil = []
-let thumb = $('div').find('.thumbnail.cover > a > img').attr('src')
-let judul = $('div').find('.thumbnail.cover > div > b').text()
-let quality = $('div').find('#mp4 > table > tbody > tr:nth-child(4) > td:nth-child(3) > a').attr('data-fquality')
-let tipe = $('div').find('#mp4 > table > tbody > tr:nth-child(3) > td:nth-child(3) > a').attr('data-ftype')
-let output = `${judul}.` + tipe
-let size = $('div').find('#mp4 > table > tbody > tr:nth-child(4) > td:nth-child(2)').text()
-let id = /var k__id = "(.*?)"/.exec(mela.result)[1]
-let res2 = await post(`https://www.y2mate.com/mates/en68/convert`, {
-    type: 'youtube',
-    _id: id,
-    v_id: ytId[1],
-    ajax: '1',
-    token: '',
-    ftype: tipe,
-    fquality: quality
-  })
-const meme = await res2.json()
-const supp = cheerio.load(meme.result)
-let link = supp('div').find('a').attr('href')
-hasil.push({ thumb, judul, quality, tipe, size, output, link})
-return hasil
 }
