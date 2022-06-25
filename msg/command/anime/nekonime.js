b5818f53c22106a3d44917b08e3e7be894a61fde
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2022-06-25 16:54:46 +0700

    Fix nekonime

--- msg/command/anime/nekonime.js
+++ msg/command/anime/nekonime.js
@@ -2,15 +2,13 @@
  * Nekonime Scrapper By @moo-d
  */
 const axios = require("axios");
-const cheerio = require("cheerio");
 
 async function result() {
   return new Promise(async(resolve, reject) => {
-    await axios.get("https://nekos.life")
+    await axios.get("https://nekos.life/api/neko")
       .then(({ data }) => {
-        $ = cheerio.load(data)
         const result = {
-          image: $("div.w3-modal > div > img").attr("src")
+          image: data.neko
         }
         resolve(result)
       }).catch(reject)
