92dc9b25483cde0dfe07fea0a3de8bf6ac3f1f44
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2022-07-02 17:49:46 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-let Baileys = require('@adiwajshing/baileys')
-let conn = new Baileys.WAConnection()
+let {
+  default: makeWASocket,
+  DisconnectReason,
+  useSingleFileAuthState,
+  fetchLatestBaileysVersion,
+  AnyMessageContent,
+  delay,
+  generateForwardMessageContent,
+  prepareWAMessageMedia,
+  generateWAMessageFromContent,
+  generateMessageID,
+  downloadContentFromMessage,
+  makeInMemoryStore,
+} = require('@adiwajshing/baileys')
 let fs = require('fs')
 let CFonts = require('cfonts')
 let figlet = require('figlet')
 let { color } = require('./lib/color')
 let package = JSON.parse(fs.readFileSync('./package.json'))
 let express = require('express')
+let pino = require("pino")
+let { Boom } = require("@hapi/boom")
+let path = require("path").join;
 let app = new express()
 let qrcode = require('qrcode')
 
@@ -16,15 +31,25 @@ app.listen(PORT, () => {
   console.log(color('Localhost is running!', 'yellow'))
 })
 
+let store = makeInMemoryStore({
+  logger: pino().child({
+    level: 'silent',
+    stream: 'store'
+  })
+})
+
 async function start() {
+  // banner
   console.log(color(figlet.textSync(`Inos Baileys\nWaBot`, 'Larry 3D'), 'cyan'))
   CFonts.say(`Created By : ${package.author} Team!`, {
     font: 'console',
     align: "center",
     gradient: ['yellow', 'yellow']
   })
-  /**
-   * Uncache if there is file change
+
+
+
+  /** Uncache if there is file change
    * @param {string} module Module name or path
    * @param {function} cb <optional>
    */
@@ -36,8 +61,7 @@ async function start() {
     })
   }
 
-  /**
-   * Uncache a module
+  /** Uncache a module
    * @param {string} module Module name or path
    */
   function uncache(module = '.') {
@@ -51,33 +75,67 @@ async function start() {
     })
   }
 
-  require('./msg/message.js')
-  nocache('./msg/message.js', module => console.log(color(`message.js is now updated!`)))
 
-  conn.autoReconnect = Baileys.ReconnectMode.onConnectionLost
-  conn.version = [2, 2140, 6]
-  conn.logger.level = 'warn'
-  conn.on('qr', async (qr) => {
-    let createQr = await qrcode.toDataURL(qr, {
-      scale: 20
+
+  // connect to whatsapp
+  async function connect() {
+    let { state, saveState } = useSingleFileAuthState(path(__dirname, `./sessions.json`), pino({ level: "silent" }));
+    let { version, isLatest } = await fetchLatestBaileysVersion();
+
+    console.log(`Using: ${version}, newer: ${isLatest}`);
+
+    let conn = makeWASocket({
+      printQRInTerminal: true,
+      auth: state,
+      logger: pino({
+        level: "silent"
+      }),
+      version
     })
-    replaceQr = await createQr.replace('data:image/png;base64,', "")
-    QR = await new Buffer.from(replaceQr, "base64")
-    console.log(color("[SYSTEM]", "green"), "Scan the QR code!")
-  })
-  fs.existsSync('./sessions.json') && conn.loadAuthInfo('./sessions.json')
-  await conn.connect({timeoutMs: 30*1000})
-  fs.writeFileSync('./sessions.json', JSON.stringify(conn.base64EncodedAuthInfo(), null, '\t'))
-  console.log(color('[BOT] Connected!', 'green'))
-  conn.on('chats-received', async ({ hasNewChats }) => {
-    console.log(color(`[SYSTEM] You have ${conn.chats.length} chats, new chats available: ${hasNewChats}`, 'magenta'))
-  })
-  conn.on('contacts-received', () => {
-    console.log(color('[SYSTEM] You have ' + Object.keys(conn.contacts).length + ' contacts', 'brown'))
-  })
-  conn.on('chat-update', async (message) => {
-    require('./msg/message.js')(conn, message)
-  })
+
+    store.bind(conn.ev)
+
+    conn.ev.on("creds.update", saveState)
+
+    conn.ev.on("connection.update", async (up) => {
+      let { lastDisconnect, connection } = up
+
+      if (connection) {
+        console.log("Connection Status: ", connection);
+      }
+      if (connection === "close") {
+        let reason = new Boom(lastDisconnect?.error)?.output?.statusCode;
+        if (reason === DisconnectReason.badSession) {
+          console.log(`Bad Session File, Please Delete sessions.json and Scan Again`);
+          conn.logout();
+        } else if (reason === DisconnectReason.connectionClosed) {
+	  console.log("Connection closed, reconnecting....");
+	  connect();
+        } else if (reason === DisconnectReason.connectionLost) {
+	  console.log("Connection Lost from Server, reconnecting...");
+	  connect();
+        } else if (reason === DisconnectReason.connectionReplaced) {
+	  console.log("Connection Replaced, Another New Session Opened, Please Close Current Session First");
+	  conn.logout();
+        } else if (reason === DisconnectReason.loggedOut) {
+	  console.log(`Device Logged Out, Please Delete ${session} and Scan Again.`);
+	  conn.logout();
+	} else if (reason === DisconnectReason.restartRequired) {
+	  console.log("Restart Required, Restarting...");
+	  connect();
+	} else if (reason === DisconnectReason.timedOut) {
+	  console.log("Connection TimedOut, Reconnecting...");
+	  connect();
+	} else {
+	  conn.end(`Unknown DisconnectReason: ${reason}|${lastDisconnect.error}`);
+	}
+      }
+    })
+    conn.ev.on('messages.upsert', async chatUpdate => {
+      require('./msg/message')(conn, chatUpdate)
+    })
+  }
+  connect()
 }
 
 app.get("/", async(req, res) => {
