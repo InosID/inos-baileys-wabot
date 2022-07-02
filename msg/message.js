a846fe1556b35af766daead898b5bb3aad533196
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2022-07-02 17:56:26 +0700

    Update message.js

--- msg/message.js
+++ msg/message.js
@@ -665,10 +665,6 @@ module.exports = msgMain = async(CXD, chatUpdate, store) => {
       case 'stiker':
       case 's':
 	if ((isImage || isQuotedImage)) {
-	  var stickerInfo = {
-	    author: "© Bot",
-	    pack: ""
-	  }
 	  var anu = args.join(' ').split('|')
 	  var satu = anu[0] !== '' ? anu[0] : stickerInfo.pack
 	  var dua = typeof anu[1] !== 'undefined' ? anu[1] : stickerInfo.author
