9e2d6a252f74fa4e9233e5bd659cc1c780188cc6
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2022-07-03 13:59:55 +0700

    Update hentai.js

--- msg/command/anime/hentai.js
+++ msg/command/anime/hentai.js
@@ -3,9 +3,20 @@
  */
 const HMfull = require('hmfull')
 
+async function getRandomPick(list) {
+  return list[Math.floor(Math.random() * list.length)]
+}
+
 async function result() {
   return new Promise(async(resolve, reject) => {
-    let res = await HMfull.Nekos.nsfw.hentai()
+    // let res;
+    // let arr = ['a', 'b']
+    // if (random == 'a') {
+    //   res = await HMfull.Nekos.nsfw.hentai()
+    // } else {
+    //   res = await HMfull.HMtai.nsfw.hentai()
+    // }
+    let res = HMfull.HMtai.nsfw.hentai()
     resolve(res.url);
   })
 }
