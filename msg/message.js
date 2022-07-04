3a32385f4a58fc6f594007969cb6e6a4d05f08d9
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2022-07-04 10:11:50 +0700

    ....

--- msg/message.js
+++ msg/message.js
@@ -664,10 +664,6 @@ module.exports = msgMain = async(CXD, chatUpdate, store) => {
       case 'stiker':
       case 's':
 	if ((isImage || isQuotedImage)) {
-	  var stickerInfo = {
-	    author: "© Bot",
-	    pack: ""
-	  }
 	  var anu = args.join(' ').split('|')
 	  var satu = anu[0] !== '' ? anu[0] : stickerInfo.pack
 	  var dua = typeof anu[1] !== 'undefined' ? anu[1] : stickerInfo.author
@@ -691,11 +687,7 @@ module.exports = msgMain = async(CXD, chatUpdate, store) => {
       break
       case 'stickergif':
         if ((isVideo || isQuotedImage)) {
-	  var stickerInfo = {
-            author: "© Bot",
-            pack: ""
-          }
-          var anu = args.join(' ').split('|')
+	  var anu = args.join(' ').split('|')
           var satu = anu[0] !== '' ? anu[0] : stickerInfo.pack
           var dua = typeof anu[1] !== 'undefined' ? anu[1] : stickerInfo.author
           var mime = (quoted.msg || quoted).mimetype || ''
