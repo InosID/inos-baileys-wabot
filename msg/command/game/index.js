ee85a4dd48c2e3edd4a4b729b4ab6c1ad27c68de
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2022-07-12 00:54:18 +0700

    Update index.js
    Signed-off-by: ナスワサルサ <75057738+moo-d@users.noreply.github.com>

--- msg/command/game/index.js
+++ msg/command/game/index.js
@@ -2,6 +2,12 @@ let fs = require('fs')
 let toMs = require('ms')
 let { tekateki } = require('./database')
 
+/** Add game to array
+ * @param {String} chatId
+ * @param {String} answer
+ * @param {String} expired
+ * @param {Object} arr
+ */
 function addGame(chatId, answer, expired, arr) {
   var obj = {
     id: chatId,
@@ -11,6 +17,11 @@ function addGame(chatId, answer, expired, arr) {
   arr.push(obj)
 }
 
+/** Get game answer
+ * @param {String} chatId
+ * @param {Object} arr
+ * @returns {String}
+ */
 function getGameAnswer(chatId, arr) {
   let found = false;
   Object.keys(arr).forEach((i) => {
@@ -23,6 +34,11 @@ function getGameAnswer(chatId, arr) {
   }
 }
 
+/** is game
+ * @param {String} chatId
+ * @param {Object} arr
+ * @returns {Boolean}
+ */
 function isGame(chatId, arr) {
   let status = false;
   Object.keys(arr).forEach((i) => {
@@ -33,6 +49,10 @@ function isGame(chatId, arr) {
   return status
 }
 
+/** Check game time
+ * @param {Function} CXD
+ * @param {Object} arr
+ */
 function checkGameTime(CXD, arr) {
   setInterval(() => {
     let position = null;
@@ -48,6 +68,11 @@ function checkGameTime(CXD, arr) {
   }, 1000);
 }
 
+/** Get game position
+ * @param {String} chatId
+ * @param {Object} arr
+ * @returns {Array}
+ */
 function getGamePosi(chatId, arr) {
   let position = null;
   Object.keys(arr).forEach((i) => {
@@ -60,10 +85,31 @@ function getGamePosi(chatId, arr) {
   }
 }
 
+/** Get game multi answer
+ * @param {String} chatId
+ * @param {String} answ
+ * @param {Object} arr
+ * @returns {Boolean}
+ */
+function getGameMultiAnswer(chatId, answ, arr) {
+  let position = false;
+  Object.keys(arr).forEach((i) => {
+    if (arr[i].answer === answ) {
+      position = i 
+    }
+  });
+  if (position !== false) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 module.exports = {
   addGame,
   getGameAnswer,
   isGame,
   checkGameTime,
-  getGamePosi
+  getGamePosi,
+  getGameMultiAnswer
 }
