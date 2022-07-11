c6aedb47ecc9c4c3011d6cb5c5cf0675ebad4f49
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2022-07-12 01:05:37 +0700

    Delete index.js
    Signed-off-by: ナスワサルサ <75057738+moo-d@users.noreply.github.com>

--- msg/command/game/database/index.js
+++
@@ -1,4 +0,0 @@
-let { readFileSync: read } = require('fs')
-
-exports.tekateki = JSON.parse(read('./msg/command/game/database/tekateki.json'))
-exports.tebakanime = JSON.parse(read('./msg/command/game/database/tebakanime.json'))
