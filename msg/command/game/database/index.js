7eecc42e4191ec89c3d1ff39ac0f1eaf13d1af20
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2022-07-12 00:13:07 +0700

    Update index.js
    Signed-off-by: ナスワサルサ <75057738+moo-d@users.noreply.github.com>

--- msg/command/game/database/index.js
+++ msg/command/game/database/index.js
@@ -1,3 +1,4 @@
 let { readFileSync: read } = require('fs')
 
 exports.tekateki = JSON.parse(read('./msg/command/game/database/tekateki.json'))
+exports.tebakanime = JSON.parse(read('./msg/command/game/database/tebakanime.json'))
